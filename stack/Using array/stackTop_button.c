f52dad459caf640874834768b541a73270a1d680
Bikram Das <96393941+Zero-day-Exploit-np@users.noreply.github.com>
2026-01-24 17:52:46 +0530

    stack

--- stack/Using array/stackTop_button.c
+++ stack/Using array/stackTop_button.c
@@ -1,66 +1,81 @@
-#include<stdio.h>
-#include<stdlib.h>
-struct stack {
+#include <stdio.h>
+#include <stdlib.h>
+struct stack
+{
     int size;
     int top;
     int *arr;
 };
 
-int isfull(struct stack * ptr){
-    if(ptr->top==ptr->size-1){
+int isfull(struct stack *ptr)
+{
+    if (ptr->top == ptr->size - 1)
+    {
         return 1;
     }
     return 0;
 }
 
-int isEmpty(struct stack *ptr){
-    if(ptr->top==-1){
-        return 1;     
+int isEmpty(struct stack *ptr)
+{
+    if (ptr->top == -1)
+    {
+        return 1;
     }
     return 0;
 }
 
-int stackTop(struct stack*ptr){
-    printf(" %d\n",ptr->arr[ptr->top]);
+int stackTop(struct stack *ptr)
+{
+    printf(" %d\n", ptr->arr[ptr->top]);
 }
-int stackbutton(struct stack * ptr){
-    printf(" %d\n",ptr->arr[0]);
+int stackbutton(struct stack *ptr)
+{
+    printf(" %d\n", ptr->arr[0]);
 }
 
-int push(struct stack * ptr,int data){
-    if(isfull(ptr)){
-        printf("stack full cannot push element\n");   
+int push(struct stack *ptr, int data)
+{
+    if (isfull(ptr))
+    {
+        printf("stack full cannot push element\n");
         return 0;
     }
 
     ptr->top++;
-    ptr->arr[ptr->top]=data;
+    ptr->arr[ptr->top] = data;
 }
-int pop(struct stack * ptr){
-    if(isEmpty(ptr)){
-        printf("stack empty cannot pop element\n");   
+int pop(struct stack *ptr)
+{
+    if (isEmpty(ptr))
+    {
+        printf("stack empty cannot pop element\n");
         return 0;
     }
     ptr->top--;
 }
 
-int peak(struct stack * ptr){
-    if(isEmpty(ptr)){
-        printf("stack empty cannot peak element\n");   
+int peak(struct stack *ptr)
+{
+    if (isEmpty(ptr))
+    {
+        printf("stack empty cannot peak element\n");
         return 0;
     }
 
-    for(int i=0; i<ptr->top+1; i++){
-        printf(" %d ",ptr->arr[i]);
+    for (int i = 0; i < ptr->top + 1; i++)
+    {
+        printf(" %d ", ptr->arr[i]);
     }
     printf("\n");
 }
 
-int main(){
-    struct stack *a= (struct stack*)malloc(sizeof(struct stack));
-    a->size=10;
-    a->top=-1;
-    a->arr=(int*)malloc(a->size* sizeof(int));
+int main()
+{
+    struct stack *a = (struct stack *)malloc(sizeof(struct stack));
+    a->size = 10;
+    a->top = -1;
+    a->arr = (int *)malloc(a->size * sizeof(int));
     push(a, 1);
     push(a, 2);
     push(a, 3);
@@ -71,13 +86,12 @@ int main(){
     push(a, 8);
     push(a, 9);
     push(a, 10);
-  
+
     peak(a);
     pop(a);
     peak(a);
     stackbutton(a);
     stackTop(a);
-    
 
-return 0;
+    return 0;
 }
---
+++ stack/Using_Linkedlist/push_pop.c
@@ -0,0 +1,80 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct Node
+{
+    int data;
+    struct Node *next;
+};
+
+struct Node *push(struct Node *ptr, int data)
+{
+    struct Node *a = (struct Node *)malloc(sizeof(struct Node));
+    a->data = data;
+    a->next = ptr;
+    return a;
+}
+
+void print(struct Node *ptr)
+{
+    while (ptr != NULL)
+    {
+        printf(" %d ", ptr->data);
+        ptr = ptr->next;
+    }
+    printf("\n");
+}
+
+int isEmpty(struct Node *ptr)
+{
+    if (ptr == NULL)
+    {
+        return 1;
+    }
+    return 0;
+}
+
+int isFull(struct Node * ptr){
+    struct Node * n=(struct Node*)malloc(sizeof(struct Node));
+    if(n < NULL){
+        return 1;
+    }
+    return 0;
+}
+
+struct Node *pop(struct Node *ptr)
+{
+    if (isEmpty(ptr))
+    {
+        printf("Stack is empty cannot pop element\n");
+        return 0;
+    }
+    struct Node *a = ptr;
+    ptr = ptr->next;
+    free(a);
+    return ptr;
+}
+int main()
+{
+    struct Node *head;
+    head = (struct Node *)malloc(sizeof(struct Node));
+
+    head->data = 1;
+    head->next = NULL;
+    head = push(head, 2);
+    head = push(head, 3);
+    head = push(head, 4);
+    head = push(head, 5);
+    head = push(head, 6);
+    head = push(head, 7);
+    head = push(head, 8);
+
+    print(head);
+    head = pop(head);
+    head = pop(head);
+    head = pop(head);
+    head = pop(head);
+
+    print(head);
+
+    return 0;
+}
