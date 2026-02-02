bea9e9132ca0517ba706b8339e3c2a37b5e42c47
Bikram Das <96393941+Zero-day-Exploit-np@users.noreply.github.com>
2026-02-02 23:34:36 +0530

    insetionshort

--- shorting/insetionshort.c
+++ shorting/insetionshort.c
@@ -1,5 +1,34 @@
-#include<stdio.h>
-int main(){
-    int arr[] = {1, 2, 3, 4, 5};
-        return 0;
+#include <stdio.h>
+void insertionshort(int *arr, int size)
+{
+    int key, j;
+    for (int i = 1; i < size; i++)
+    {
+        key = arr[i];
+        j = i - 1;
+
+        while (j >= 0 && arr[j] > key)
+        {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = key;
+    }
+}
+void print(int *arr, int size)
+{
+    for (int i = 0; i < 5; i++)
+    {
+        printf(" %d ", arr[i]);
+    }
+    printf("\n");
+}
+int main()
+{
+    int arr[] = {1, 2, 6, 4, 5};
+    print(arr, 5);
+    insertionshort(arr, 5);
+    print(arr, 5);
+
+    return 0;
 }
