1756f192c15a24f2a50ea84ba7240f7496fa1582
Bikram Das <96393941+Zero-day-Exploit-np@users.noreply.github.com>
2026-02-04 00:42:12 +0530

    shorting

--- shorting/selectionshort.c
+++ shorting/selectionshort.c
@@ -1,19 +1,35 @@
-#include<stdio.h>
-int main(){
-    int a[]={5,4,3,2,1,0};
-    int size=6;
-    int key,j;
+#include <stdio.h>
 
-    for(int i=0; i<size; i++){
-        key=a[i];
-        j=i;
-        while(j<size){
-            if(a[j]<key){
-                key=a[j];
+int main()
+{
+    int a[] = {5, 9, 3, 2, 1, 0};
+    int size = 6;
+
+    for (int i = 0; i < size - 1; i++)
+    {
+        int minIndex = i;
+
+        for (int j = i + 1; j < size; j++)
+        {
+            if (a[j] < a[minIndex])
+            {
+                minIndex = j;
             }
+        }
+
+        // swap only once
+        if (minIndex != i)
+        {
+            int temp = a[i];
+            a[i] = a[minIndex];
+            a[minIndex] = temp;
         }
     }
 
+    for (int i = 0; i < size; i++)
+    {
+        printf(" %d", a[i]);
+    }
 
-return 0;
+    return 0;
 }
