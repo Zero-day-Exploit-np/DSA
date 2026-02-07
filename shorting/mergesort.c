7481b3c7aa364dd37420565cd7d495bd75ba59c7
Bikram Das <96393941+Zero-day-Exploit-np@users.noreply.github.com>
2026-02-07 18:56:05 +0530

    mergesort

--- shorting/mergesort.c
+++ shorting/mergesort.c
@@ -1,53 +1,65 @@
 #include <stdio.h>
-
-void merge(int arr[], int low, int mid, int high)
+void merge(int *arr, int mid, int low, int high)
 {
-    int temp[high - low + 1];
-    int i = low, j = mid + 1, k = 0;
-
+    int array[high+1];
+    int i=low, k;
+    k = low;
+    int j = mid + 1;
     while (i <= mid && j <= high)
     {
         if (arr[i] < arr[j])
-            temp[k++] = arr[i++];
+        {
+            array[k] = arr[i];
+            k++;
+            i++;
+        }
         else
-            temp[k++] = arr[j++];
+        {
+            array[k] = arr[j];
+            k++;
+            j++;
+        }
     }
-
     while (i <= mid)
-        temp[k++] = arr[i++];
-
+    {
+        array[k] = arr[i];
+        k++;
+        i++;
+    }
     while (j <= high)
-        temp[k++] = arr[j++];
-
-    for (i = low; i <= high; i++)
-        arr[i] = temp[i - low];
+    {
+        array[k] = arr[j];
+        k++;
+        j++;
+    }
+    for (int i = low; i <= high; i++)
+    {
+        arr[i] = array[i];
+    }
 }
-
-void mergesort(int arr[], int low, int high)
+void mergesort(int *arr, int low, int high)
 {
+    int mid;
     if (low < high)
     {
-        int mid = (low + high) / 2;
+        mid = (low + high) / 2;
         mergesort(arr, low, mid);
         mergesort(arr, mid + 1, high);
-        merge(arr, low, mid, high);
+        merge(arr, mid, low, high);
     }
 }
-
-void print(int arr[], int size)
+void print(int *a, int size)
 {
     for (int i = 0; i < size; i++)
-        printf("%d ", arr[i]);
+    {
+        printf(" %d", a[i]);
+    }
     printf("\n");
 }
-
 int main()
 {
     int arr[] = {9, 14, 4, 8, 7, 5, 6};
-    int size = 7;
-
-    mergesort(arr, 0, size - 1);
-    print(arr, size);
-
+    mergesort(arr, 0, 6);
+    print(arr, 7);
     return 0;
 }
