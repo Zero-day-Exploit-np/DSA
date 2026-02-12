7320a8fe8c596ecc9a6ae11a880d081a75b1c0c6
Bikram Das <96393941+Zero-day-Exploit-np@users.noreply.github.com>
2026-02-12 23:32:26 +0530

    AVL tree

--- Tree/AVL_Tree.c
+++ Tree/AVL_Tree.c
@@ -27,9 +27,35 @@ struct Node *NodeCreate(int data)
     return node;
 }
 
-int getBalanceFactor(struct Node * n){
-    
+int getBalanceFactor(struct Node *n)
+{
+    if (n == NULL)
+    {
+        return 0;
+    }
+    return height(n->left) - height(n->right);
 }
+
+struct Node *leftRotation(struct Node *y)
+{
+    struct Node *x = y->left;
+    struct Node *t2 = x->right;
+    x->right = y;
+    y->left = t2;
+    y->height = max(height(y->right), height(y->left));
+    x->height = max(height(x->right), height(x->left));
+}
+
+struct Node *RightRotation(struct Node *x)
+{
+    struct Node *y = x->left;
+    struct Node *t2 = ->right;
+    x->right = y;
+    y->left = t2;
+    y->height = max(height(y->right), height(y->left));
+    x->height = max(height(x->right), height(x->left));
+}
+
 int main()
 {
 
