a8c33ff54cae2939455208bd4f64f51bebbb55ca
Bikram Das <96393941+Zero-day-Exploit-np@users.noreply.github.com>
2026-02-12 23:59:13 +0530

    avl

--- Tree/AVL_Tree.c
+++ Tree/AVL_Tree.c
@@ -36,24 +36,58 @@ int getBalanceFactor(struct Node *n)
     return height(n->left) - height(n->right);
 }
 
-struct Node *leftRotation(struct Node *y)
+struct Node *rightRotation(struct Node *y)
 {
     struct Node *x = y->left;
     struct Node *t2 = x->right;
     x->right = y;
     y->left = t2;
-    y->height = max(height(y->right), height(y->left));
-    x->height = max(height(x->right), height(x->left));
+    y->height = max(height(y->right), height(y->left))+1;
+    x->height = max(height(x->right), height(x->left))+1;
+    return x;
+
 }
 
-struct Node *RightRotation(struct Node *x)
+struct Node *leftRotation(struct Node *x)
 {
-    struct Node *y = x->left;
-    struct Node *t2 = ->right;
-    x->right = y;
-    y->left = t2;
-    y->height = max(height(y->right), height(y->left));
-    x->height = max(height(x->right), height(x->left));
+    struct Node *y = x->right;
+    struct Node *t2 = y->left;
+    y->left = x;
+    x->right = t2;
+    y->height = max(height(y->right), height(y->left))+1;
+    x->height = max(height(x->right), height(x->left))+1;
+    return y;
+
+}
+
+struct Node * insert(struct Node * node, int data){
+    if(node==NULL){
+        return (NodeCreate(data));
+    }
+    if(data<node->data){
+        node->left= insert(node->left,data);
+    }else if (data >node->data)
+    {
+        node->right=insert(node->right,data);
+        return node;
+        node->height=1+max(height(node->left),height(node->right));
+        int bf =height(node);
+
+        if(bf>1&& data<node->left->data){
+            rightRotation(node);
+        }
+
+        if (bf <- 1 && data >node->right->data)
+        {
+            leftRotation(node);
+        }
+
+        if (bf > 1 && data < node->left->data)
+        {
+            rightRotation(node);
+        }
+    }
+    
 }
 
 int main()
